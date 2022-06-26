941a44c53b9133ec97b77410b3ad3df12f309366
vavdb <vincent@vandenbraken.com>
2022-06-27 00:13:45 +0200

    URfit Theming

--- assets/js/main.js
+++ assets/js/main.js
@@ -252,3 +252,35 @@
 		}
 
 })(jQuery);
+
+$(window).scroll(function() {
+  
+	// selectors
+	var $window = $(window),
+		$body = $('.bg,#nav'),
+		$panel = $('.scroll-color');
+	
+	// Change 33% earlier than scroll position so colour is there when you arrive.
+	var scroll = $window.scrollTop();// + ($window.height() / 3);
+   
+	$panel.each(function () {
+	  var $this = $(this);
+	  
+	  // if position is within range of this panel.
+	  // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+	  // Remember we set the scroll to 33% earlier in scroll var.
+	  if (($this.position().top) <= scroll && $this.is(":visible")) {
+			
+		// Remove all classes on body with color-
+		$body.removeClass(function (index, css) {
+		  return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+		});
+		 
+		// Add class of currently active div
+		//$body.css("background-color", $(this).data('scroll-color'));
+		//console.log('changing color ',$this.children('header').text() , $(this).data('scroll-color'))
+		$body.addClass('color-' + $(this).data('scroll-color'));
+	  }
+	});    
+	
+  }).scroll();
